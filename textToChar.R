855eff852e47337e7527b5c91a8fbee26fef958c
wren <33828481+skrenata@users.noreply.github.com>
2018-12-10 18:50:38 +0000

    Update textToChar.R

--- textToChar.R
+++ textToChar.R
@@ -1,5 +1,12 @@
 textToChar <- function(...){
-    ## converts comma-separated text into a character array
+    #######################################################
+    ## Converts comma-separated text into a character array
+    ##
+    ## Useful when you need to name / rename lots of things 
+    ## and can't be bothered to type quotes around each entry
+    ## Example:
+    ## textToChar(name1, name2, name_three, name/4) == 
+    ##            c("name1", "name2", "name_three", "name/4")
     dots <- substitute(list(...))[-1]
     dots <- sapply(dots, deparse)
     return(dots)
