13675cff4f7d149f7e4db2ca4c95a02b0802aff2
wren <33828481+skrenata@users.noreply.github.com>
2018-12-11 12:12:19 +0000

    Create yIntoOrderX.R

---
+++ yIntoOrderX.R
@@ -0,0 +1,34 @@
+yIntoOrderX <- function(x,y) {
+	#########################################################
+  ## function to put vector y in the same order as vector x
+	## for example,
+	## x <- c("b","c","a","d")
+	## y <- c("DD","AA","BB","CC")
+	## => yIntoOrderX(x, y) == c("BB","CC","AA","DD")
+  #########################################################
+	
+	rankOfX <- rank(x, ties.method = "last")
+	sortedY <- sort(y)
+	reorderedY <- sortedY[rankOfX]
+	
+	return(reorderedY)
+}
+
+#################### alternative ##########################
+# yIntoOrderX <- function(x,y) {
+# 	#######################################################
+#   ## function to get in which order y must be put to be
+#   ## in the same order as vector x
+# 	## for example,
+# 	## x <- c("b","c","a","d")
+# 	## y <- c("D","A","B","C")
+# 	## => yIntoOrderX(x, y) == c(3, 4, 2, 1)
+#   ## => y[yIntoOrderX(x, y)] == c("BB","CC","AA","DD")
+#   #######################################################
+#   
+# 	rankOfX <- rank(x, ties.method = "last")
+# 	orderOfY <- order(y)
+# 	reorderY <- orderOfY[rankOfX]
+# 	
+# 	return(reorderY)
+# }
