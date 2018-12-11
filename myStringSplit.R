9c63ae79e02bf9fac2e3c378f6f5e688dd5c186c
wren <33828481+skrenata@users.noreply.github.com>
2018-12-11 17:10:20 +0000

    Update myStringSplit.R

--- myStringSplit.R
+++ myStringSplit.R
@@ -1,21 +1,19 @@
-function(x,
-         pat,
-         pos){
+myStringSplit <- function(x,
+			  pat,
+			  pos){
 	############################################################################
 	## custom function to split string at a particular pattern and return
 	## a specific position before / after that pattern
 	############################################################################
-	##
 	## input:
 	## x -> character array
 	## pat -> pattern at which split has to happen
 	## pos -> which position to return (1 == before pat, 2 == after pat)
-	##
 	############################################################################
-	##
-	## NOTE: this will probably not work if pat appears more than once in each
-	## entry of array x
-	##
+	## example:
+	## myStringSplit(x="test_sample_string", pat="_", pos=1) == "test"
+	## myStringSplit(x="test_sample_string", pat="_", pos=2) == "sample"
+	## myStringSplit(x="test_sample_string", pat="_", pos=3) == "string"
 	############################################################################
 	
 	originalSplit <- strsplit(x, split=pat)
